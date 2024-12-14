b7696eeb1f0e324a301c0acd449a66d2bb9ac542
Fatih <fatih_0226@outlook.com>
2024-12-15 00:25:58 +0300

    uploads yolu düzeltildi

--- src/main/java/com/example/blogv1/Config/WebConfig.java
+++ src/main/java/com/example/blogv1/Config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // /upload/** yolunu /var/www/uploads/ dizinine yönlendir
         registry.addResourceHandler("/upload/**")
-                .addResourceLocations("file:/var/www/uploads/")
+                .addResourceLocations("file:/var/www/upload/")
                 .setCachePeriod(3600) // İsteğe bağlı: Cache süresini ayarlar (saniye cinsinden)
                 .resourceChain(true);
     }
