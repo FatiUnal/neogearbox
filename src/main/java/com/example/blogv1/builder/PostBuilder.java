4e3056292a2773dc738b628b7dd97e015f73a2c0
Fatih <fatih_0226@outlook.com>
2024-12-15 12:02:31 +0300

    asd

--- src/main/java/com/example/blogv1/builder/PostBuilder.java
+++ src/main/java/com/example/blogv1/builder/PostBuilder.java
@@ -5,18 +5,31 @@ import com.example.blogv1.entity.post.Image;
 import com.example.blogv1.entity.post.Post;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 @Component
 public class PostBuilder {
 
     public PostSmallDto postToPostSmallDto(Post post) {
+        String coverImage = "";
+        List<String> images = new ArrayList<>();
+
+        if (post.getCoverImage().getFilename() != null) {
+            coverImage = post.getCoverImage().getFilename();
+        }
+
+        if (post.getImages() != null) {
+            post.getImages().forEach(image -> images.add(image.getFilename()));
+        }
+
         return new PostSmallDto(
                 post.getId(),
                 post.getTitle(),
                 post.getContent(),
-                post.getCoverImage() != null ? post.getCoverImage().getFilename() : "",
-                post.getImages() != null ? post.getImages().stream().map(Image::getFilename).toList() : Collections.emptyList()
+                coverImage,
+                images
         );
     }
 }
