c33d8ccba18f5d8025bc9b4edf28c77f6b316773
Fatih <fatih_0226@outlook.com>
2024-12-15 12:22:42 +0300

    asd

--- src/main/java/com/example/blogv1/builder/PostBuilder.java
+++ src/main/java/com/example/blogv1/builder/PostBuilder.java
@@ -13,15 +13,29 @@ import java.util.List;
 public class PostBuilder {
 
     public PostSmallDto postToPostSmallDto(Post post) {
+        System.out.println("1");
         String coverImage = "";
+        System.out.println("2");
+
         List<String> images = new ArrayList<>();
+        System.out.println("3");
+
 
         if (post.getCoverImage().getFilename() != null) {
+            System.out.println("4");
             coverImage = post.getCoverImage().getFilename();
+            System.out.println("5");
+
         }
+        System.out.println("6");
+
 
         if (post.getImages() != null) {
+            System.out.println("7");
+
             post.getImages().forEach(image -> images.add(image.getFilename()));
+            System.out.println("8");
+
         }
 
         return new PostSmallDto(
